116ab64b5c176b6eb8aab6532f889c792d1dc9a5
Ricadamm <adamrizky742@gmail.com>
2024-11-25 16:01:33 +0700

    Update Problem2.cpp

--- Problem2/Problem2.cpp
+++ Problem2/Problem2.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int factorial(int x){
-    if (x<=1){
-        return 1;
+    if (x<=1){ // checks if x is lower or equal to 1
+        return 1; // return back to int factorial
     }
     else
-    return x*factorial(x-1);
+    return x*factorial(x-1); // calculate x*factorial(x-1)
 }  
 int main(){
     int x;
-    cout << "Enter number you want to be factorised: ";
-    cin >> x;
-    cout << "The factorial of " << x << " is " << factorial(x) << endl;
+    cout << "Enter number you want to be factorised: "; // Dispalys a question to the user
+    cin >> x; // Inputs as x
+    cout << "The factorial of " << x << " is " << factorial(x) << endl; // Displays the result of the factorial
     
     return 0;
 }
