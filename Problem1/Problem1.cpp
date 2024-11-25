15226be96ffbc5a79a8ee7deaa07371a170b5937
Ricadamm <adamrizky742@gmail.com>
2024-11-25 15:46:01 +0700

    Update Problem1.cpp

--- Problem1/Problem1.cpp
+++ Problem1/Problem1.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+using namespace std;
 
 int main(){
-  string str, rev
-  cout << "Enter the word/string";
+  string str, rev;
+  cout << "Enter the word/string: ";
   cin >> str;
   rev = str;
+  reverse(rev.begin(),rev.end());
+  if (str == rev)
+  cout << "This word is a palindrome";
+  else
+  cout << "This word is not a palindrome";
+  cout << endl;
+  
+  return 0;
+}
 
   
